c0bc21399672a85a1547c2ace51737f7237b9fde
Альберт Хабиров <alb.xab@yandex.ru>
2026-02-10 11:49:30 +0500

    finals commit in this lessons

--- Banking_project/scr/widget.py
+++ Banking_project/scr/widget.py
@@ -2,6 +2,10 @@ from scr.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(input_date):
+
+    ''' Функция принимающая название счёта или карты и
+     её номер и маркирующая их, чтобы затем вернуть'''
+
     date_base = input_date.split()
     names_date = ' '.join(date_base[:-1])
     numbers_date = date_base[-1]
@@ -11,7 +15,12 @@ def mask_account_card(input_date):
         mask_ = get_mask_card_number(numbers_date)
     return names_date+ ' ' + mask_
 
+
 def get_date(date):
+
+    '''Функция для получения упрощённой даты из более подробного формата
+    исключая тем самым точное время операции'''
+
     worker_date = date.split('T')
     year, month, day = worker_date[0].split('-')
     return f'{day}.{month}.{year}'
